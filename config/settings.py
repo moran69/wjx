f93cca31f727d78af21acbc30166caa04a656330
suiyuan <39113888+moran69@users.noreply.github.com>
2025-02-19 22:06:03 +0800

    Create settings.py
    配置信息

---
+++ config/settings.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ['TELEGRAM_BOT_TOKEN'] = "your_telegram_bot_token"
+os.environ['TELEGRAM_CHAT_ID'] = "your chatid"
+os.environ['QUESTIONNAIRE_URL'] = "问卷星统计分析页面呈现链接"///例如"https://www.wjx.cn/report/302085648.aspx"
